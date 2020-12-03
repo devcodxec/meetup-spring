79624898c2907d788c25c47967c409cfe70db3ea
dennys <dennys.landy@hotmail.com>
2020-12-03 20:19:28 +0100

    Se configura los entities Tema-TemaUsuario #5

--- src/main/java/com/proyecto/meetup/entities/TemaUsuario.java
+++ src/main/java/com/proyecto/meetup/entities/TemaUsuario.java
@@ -1,4 +1,59 @@
 package com.proyecto.meetup.entities;
 
-public class TemaUsuario {
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+public class TemaUsuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="Id_TemaUsuario")
+    private Long idTemaUsuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="Id_Tema")
+    private Tema tema;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="Id_Usuario")
+    private Usuario usuario;
+
+    public TemaUsuario(){
+
+    }
+
+    public Long getIdTemaUsuario() {
+        return idTemaUsuario;
+    }
+
+    public void setIdTemaUsuario(Long idTemaUsuario) {
+        this.idTemaUsuario = idTemaUsuario;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public String toString() {
+        return "TemaUsuario{" +
+                "idTemaUsuario=" + idTemaUsuario +
+                ", tema=" + tema +
+                ", usuario=" + usuario +
+                '}';
+    }
 }
