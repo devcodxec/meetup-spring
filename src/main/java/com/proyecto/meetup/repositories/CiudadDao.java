ac2e4962d18851e41816b32f5d6cca1f1f56ae35
dennys <dennys.landy@hotmail.com>
2020-12-06 21:24:50 +0100

    Se modifican las clases para crear las funciones crud de Ciudad #9

--- src/main/java/com/proyecto/meetup/repositories/CiudadDao.java
+++ src/main/java/com/proyecto/meetup/repositories/CiudadDao.java
@@ -3,5 +3,9 @@ package com.proyecto.meetup.repositories;
 import com.proyecto.meetup.entities.Ciudad;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface CiudadDao extends CrudRepository<Ciudad,Long> {
+
+   List<Ciudad> findAll();
 }
