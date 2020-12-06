0887d73662b63f5eb027f489d72c73555d78bb7d
dennys <dennys.landy@hotmail.com>
2020-12-06 01:46:59 +0100

    Se crean los controllers de la capa businnes_controllers #9

---
+++ src/main/java/com/proyecto/meetup/business_controlers/CiudadController.java
@@ -0,0 +1,8 @@
+package com.proyecto.meetup.business_controlers;
+
+import com.proyecto.meetup.repositories.CiudadDao;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CiudadController {
+}
