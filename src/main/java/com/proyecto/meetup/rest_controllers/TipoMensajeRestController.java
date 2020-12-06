731544f269006fe44c6a9622a9babc1348d3d182
dennys <dennys.landy@hotmail.com>
2020-12-06 21:13:40 +0100

    Se modifica el nombre de la clase #9

--- src/main/java/com/proyecto/meetup/rest_controllers/TipoMensajeRestController.java
+++ src/main/java/com/proyecto/meetup/rest_controllers/TipoMensajeRestController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(TipoMensajeController.TIPO_MENSAJES)
-public class TipoMensajeController {
+@RequestMapping(TipoMensajeRestController.TIPO_MENSAJES)
+public class TipoMensajeRestController {
 
     public static final String TIPO_MENSAJES = "/tipoMensajes";
 }
